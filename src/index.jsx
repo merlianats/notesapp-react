95d06b522f6a10896c694a24421ca9c12772828b
merlianats <merlianatris@gmail.com>
2025-06-17 19:39:52 +0700

    first project commit

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+
+const element = <h1>Hello, Good Morning!</h1>
+
+const root = createRoot(document.getElementById('root'));
+root.render(element);
